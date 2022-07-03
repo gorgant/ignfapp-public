95a5379185c7b72b8445c9715f03f211f3ba1659
gorgant t15g <gorgant@gorgant.com>
2022-07-03 09:09:07 -0400

    upgrade to 0.0.7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { RootStoreState, UiStoreActions, UiStoreSelectors } from './root-store';
 })
 export class AppComponent {
   TITLE = 'ignfapp-public';
-  APP_VERSION = '0.0.6'
+  APP_VERSION = '0.0.7'
 
   showNavBar$!: Observable<boolean>;
 
