b94b4fd85998a3109ba7de7fe8880541e56e334e
gorgant <24444990+gorgant@users.noreply.github.com>
2024-09-10 14:01:59 +0300

    update angular, firebase, and env configs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    projectId: 'ignfapp-public',
-    appId: '1:664901820175:web:3a79a47813307fe82ee87a',
-    storageBucket: 'ignfapp-public.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyDZ0OLZnU6ng8-9PJsR5kCBOhASg-Z4oDg',
-    authDomain: 'ignfapp-public.firebaseapp.com',
-    messagingSenderId: '664901820175',
-    measurementId: 'G-203MP9XQNX',
+    apiKey: "AIzaSyDZ0OLZnU6ng8-9PJsR5kCBOhASg-Z4oDg",
+    authDomain: "ignfapp-public.firebaseapp.com",
+    projectId: "ignfapp-public",
+    storageBucket: "ignfapp-public.appspot.com",
+    messagingSenderId: "664901820175",
+    appId: "1:664901820175:web:a3c3b8888405ce992ee87a",
+    measurementId: "G-HX6QSYVBL6"
   },
   reCaptchaEnterpriseProviderKey: '6LeOAQwoAAAAAHsoaUSw9B7RLIPlpTkWoaVW7kyv',
   appCheckDebugTokenActive: false
