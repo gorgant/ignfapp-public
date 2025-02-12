747326cbd3282a0eab6fb759a58011467172e12d
gorgant <24444990+gorgant@users.noreply.github.com>
2025-02-12 16:35:30 +0300

    update package lock to hopefully fix deploy error

--- src/app/core/services/ui.service.ts
+++ src/app/core/services/ui.service.ts
@@ -19,7 +19,7 @@ import { SwUpdate } from '@angular/service-worker';
 })
 export class UiService {
 
-  private APP_VERSION = '0.3.1';
+  private APP_VERSION = '0.3.2';
 
   private history: string[] = [];
   private $privateHideNavBar = signal(true);
